2f5d1fe4f39003d3af273a363d03ceb5fef22b6b
Taegan Loyzelle <tloyzelle@gmail.com>
2021-06-27 21:25:24 -0400

    adding packages needed for application within index.js

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
+const path = require('path');
 
 // TODO: Create an array of questions for user input
 const questions = [];
