f54d3934e1a291656fd3fe01659098d041970239
Taegan Loyzelle <tloyzelle@gmail.com>
2021-06-27 21:46:47 -0400

    adding questions to be asked to user

--- index.js
+++ index.js
@@ -5,7 +5,48 @@ const generateMarkdown = require('./utils/generateMarkdown');
 const path = require('path');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [ {
+    type: "input",
+    name: "Title",
+    message: "What is the title to your project/application?"
+},
+{
+    type: "input",
+    name: "Description",
+    message: "Describe your project/application, and how it works.",
+},
+{
+    type: "input",
+    name: "Use",
+    message: "Describe how to use your project/application."
+},
+{
+    type: "input",
+    name: "Installation",
+    message: "How can your project/application be installed?",
+},
+{
+    type: "input",
+    name: "Guide",
+    message: "What is the protocol to contributing to this project/application?",
+},
+{
+    type: "checkbox",
+    name: "License",
+    message: "Which license would you use?",
+    choices: ["MIT", "APACHE", "BSD", "None", "WTFPL"],
+},
+{
+    type: "input",
+    name: "Github",
+    message: "What is your Github username?",
+},
+{
+    type: "input",
+    name: "Email",
+    message: "What is your preferred email address?",
+}
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
