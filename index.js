97b8a7c065b3f7f23c1217bcfc97e933ffe99ae7
Taegan Loyzelle <tloyzelle@gmail.com>
2021-06-29 18:36:36 -0400

    doing bug fixes making it work

--- index.js
+++ index.js
@@ -1,68 +1,80 @@
-// TODO: Include packages needed for this application
-const inquirer = require('inquirer');
-const fs = require('fs');
-const generateMarkdown = require('./utils/generateMarkdown');
-const path = require('path');
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
+const writeFileAsync = util.promisify(fs.writeFile);
 
-// TODO: Create an array of questions for user input
-//Questions involving the title, description, use, and installation of the project/application
-//Also involving what the guidelines are, what license is used, github username, and email
-const questions = [ {
-    type: "input",
-    name: "Title",
-    message: "What is the title to your project/application?"
-},
-{
-    type: "input",
-    name: "Description",
-    message: "Describe your project/application, and how it works.",
-},
-{
-    type: "input",
-    name: "Use",
-    message: "Describe how to use your project/application."
-},
-{
-    type: "input",
-    name: "Installation",
-    message: "How can your project/application be installed?",
-},
-{
-    type: "input",
-    name: "Guide",
-    message: "What is the protocol to contributing to this project/application?",
-},
-{
-    type: "checkbox",
-    name: "License",
-    message: "Which license would you use?",
-    choices: ["MIT", "APACHE", "BSD", "None", "WTFPL"],
-},
-{
-    type: "input",
-    name: "Github",
-    message: "What is your Github username?",
-},
-{
-    type: "input",
-    name: "Email",
-    message: "What is your preferred email address?",
-}
-];
+const promptUser = () => {
+  return inquirer.prompt([ {
+//Questions given to user during the prompt
+        type: "input",
+        name: "title",
+        message: "What is the title to your project/application?"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Describe your project/application, and how it works.",
+    },
+    {
+        type: "input",
+        name: "use",
+        message: "Describe how to use your project/application."
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "How can your project/application be installed?",
+    },
+    {
+        type: "input",
+        name: "guide",
+        message: "What is the protocol to contributing to this project/application?",
+    },
+    {
+        type: "checkbox",
+        name: "license",
+        message: "Which license would you use?",
+        choices: ["MIT", "APACHE", "BSD", "None", "WTFPL"],
+    },
+    {
+        type: "input",
+        name: "github",
+        message: "What is your Github username?",
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is your preferred email address?",
+    
+    },
+  ])
+};
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {
-    return fs.writeFileSync(path.join(process.cwd(), fileName), data);
-}
+//Generating the ReadMe with categories using the answers from prompt
+const generateReadme = (answers) =>
+  `# ${answers.title}
+## Description: ${answers.description}
+## 1. Installation: 
+    ${answers.installation}  
+## 2. Protocal to edit: 
+    ${answers.guide}
+## 3. Usage: 
+    ${answers.use} 
+## 4. License Used:
+    ${answers.license}
+### GitHub: https://github.com/${answers.github}
+### Email:  <a href="mailto:${answers.email}" hspace="20">${
+    answers.email
+  }</a>
+`;
 
-// TODO: Create a function to initialize app
-//uses prompts to create a readme using the responses
-function init() {
-    inquirer.prompt(questions).then((inquirerResponse) => {
-        console.log ('generating README.md...');
-        writeToFile("README.md", generateMarkdown({...inquirerResponse}));
+const build = () => {
+  promptUser()
+    .then((answers) => {
+      writeFileAsync("myreadme.md", generateReadme(answers));
     })
-}
+    .then(() => console.log("Successfully wrote to myreadme.md"))
+    .catch((err) => console.error(err));
+};
 
-// Function call to initialize app
-init();
+build();
